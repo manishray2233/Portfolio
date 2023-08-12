c11c251af846bc11ce0b599da24f015d13a29b68
Manish Kumar Ray <manishray2233@gmail.com>
2023-08-12 20:50:31 +0530

    first commit

---
+++ src/components/HeroSection/index.js
@@ -0,0 +1,106 @@
+import React from "react";
+import styled from "styled-components";
+
+const HeroContainer = styled.div`
+  background-color: ${({ theme }) => theme.card_light};
+  display: flex;
+  justify-content: center;
+  position: relative;
+  padding: 80px 30px;
+
+  @media screen and (max-width: 960px) {
+    padding: 66px 16px;
+  }
+
+  @media screen and (max-width: 640px) {
+    padding: 32px 12px;
+  }
+
+  z-index: 1;
+  clip-path: polygon(0 0, 100% 0, 100% 100%, 70% 95%, 0 100%);
+`;
+
+const HeroBg = styled.div`
+  position: absolute;
+  display: flex;
+  justify-content: end;
+  top: 50%;
+  right: 0;
+  bottom: 0;
+  left: 50%;
+  overflow: hidden;
+  width: 100%;
+  height: 100%;
+  padding: 0 30px;
+  -webkit-transform: translateX(-50%) translateY(-50%);
+  transform: translateX(-50%) translateY(-50%);
+
+  @media screen and (max-width: 960px) {
+    padding: 0 0px;
+    justify-content: center;
+  }
+`;
+
+const HeroInnerContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  max-width: 1100px;
+  @media screen and (max-width: 960px) {
+    flex-direction: column;
+  }
+`;
+
+const HeroLeftContainer = styled.div`
+  width: 100%;
+  order: 1;
+  @media screen and (max-width: 960px) {
+    order: 2;
+    margin-bottom: 32px;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+  }
+  @media screen and (max-width: 640px) {
+    order: 2;
+    margin-bottom: 32px;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+  }
+`;
+
+const HeroRightContainer = styled.div`
+  width: 100%;
+  order: 2;
+  display: flex;
+  justify-content: end;
+  gap: 12px;
+  @media screen and (max-width: 960px) {
+    order: 1;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 80px;
+  }
+  @media screen and (max-width: 640px) {
+    order: 1;
+    margin-bottom: 30px;
+  }
+`;
+
+const Hero = () => {
+  return (
+    <div id="about">
+      <HeroContainer>
+        <HeroBg></HeroBg>
+        <HeroInnerContainer>
+          <HeroLeftContainer></HeroLeftContainer>
+          <HeroRightContainer></HeroRightContainer>
+        </HeroInnerContainer>
+      </HeroContainer>
+    </div>
+  );
+};
+
+export default Hero;
