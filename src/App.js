11a857632290f5ac94175777606d71c12f28cc69
Manish Kumar Ray <manishray2233@gmail.com>
2023-08-17 10:03:17 +0530

    Done

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import "./App.css";
-import styled, { ThemeProvider } from "styled-components";
-import { BrowserRouter as Router } from "react-router-dom";
-import { darkTheme } from "./utils/Themes";
+import { ThemeProvider } from "styled-components";
+import { useState, useEffect } from "react";
+import { darkTheme, lightTheme } from "./utils/Themes.js";
 import Navbar from "./components/Navbar";
-import Hero from "./components/HeroSection";
+import "./App.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import HeroSection from "./components/HeroSection";
+import About from "./components/About";
 import Skills from "./components/Skills";
-import Education from "./components/Education";
-import Experience from "./components/Experience";
 import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Experience from "./components/Experience";
+import Education from "./components/Education";
+import ProjectDetails from "./components/ProjectDetails";
+import styled from "styled-components";
 
 const Body = styled.div`
   background-color: ${({ theme }) => theme.bg};
   width: 100%;
-  height: 100%;
   overflow-x: hidden;
 `;
 
@@ -28,23 +33,31 @@ const Wrapper = styled.div`
       rgba(0, 70, 209, 0.15) 100%
     );
   width: 100%;
-  clip-path: polygon(0 0, 100% 0, 100% 100%, 0 calc(100%-5w));
+  clip-path: polygon(0 0, 100% 0, 100% 100%, 30% 98%, 0 100%);
 `;
 function App() {
+  const [darkMode, setDarkMode] = useState(true);
+  const [openModal, setOpenModal] = useState({ state: false, project: null });
+  console.log(openModal);
   return (
-    <ThemeProvider theme={darkTheme}>
+    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
       <Router>
         <Navbar />
         <Body>
-          <Hero />
+          <HeroSection />
           <Wrapper>
             <Skills />
             <Experience />
           </Wrapper>
-          <Projects />
+          <Projects openModal={openModal} setOpenModal={setOpenModal} />
           <Wrapper>
             <Education />
+            <Contact />
           </Wrapper>
+          <Footer />
+          {openModal.state && (
+            <ProjectDetails openModal={openModal} setOpenModal={setOpenModal} />
+          )}
         </Body>
       </Router>
     </ThemeProvider>
--- src/components/Projects/ProjectsStyle.js
+++ src/components/Projects/ProjectsStyle.js
@@ -14,6 +14,10 @@ export const Container = styled.div`
   z-index: 1;
   align-items: center;
   clip-path: polygon(0 0, 100% 0, 100% 100%, 100% 98%, 0 100%);
+
+  @media (max-width: 690px) {
+    clip-path: polygon(0 0, 100% 0, 100% 100%, 147% 99%, 0 100%);
+  }
 `;
 
 export const Wrapper = styled.div`
