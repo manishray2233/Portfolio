e65be5649c6c38e3209c6657b18aed1eea3fa239
Manish Kumar Ray <97890880+manishray2233@users.noreply.github.com>
2025-09-22 16:19:32 +0530

    Updated Resume

--- src/data/constants.js
+++ src/data/constants.js
@@ -11,7 +11,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/manishray2233",
   resume:
-    "https://drive.google.com/file/d/1WnvByNINCD7-3NcKgPLV4WiYkEag9BN5/view?usp=sharing",
+    "https://drive.google.com/file/d/1YE6q2Dvr0-ZoAzr4E9W6ESqwcKvE5c_F/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/manish-kumar-ray/",
   // twitter: "https://twitter.com/RishavChanda",
   // insta: "https://www.instagram.com/rishav_chanda/",
