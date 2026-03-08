eb5a4600d625dd14092a701e758f98661570e763
Manish Kumar Ray <97890880+manishray2233@users.noreply.github.com>
2026-03-08 17:19:35 +0530

    Update index.js

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -10,13 +10,11 @@ import {
   ButtonContainer,
   MobileIcon,
   MobileMenu,
-  MobileNavLogo,
   MobileLink,
 } from "./NavbarStyledComponent";
 import { DiCssdeck } from "react-icons/di";
 import { FaBars } from "react-icons/fa";
 import { Bio } from "../../data/constants";
-import { Close, CloseRounded } from "@mui/icons-material";
 import { useTheme } from "styled-components";
 
 const Navbar = () => {
@@ -25,19 +23,17 @@ const Navbar = () => {
   return (
     <Nav>
       <NavbarContainer>
-        <NavLogo to="/">
-          <a
-            style={{
-              display: "flex",
-              alignItems: "center",
-              color: "white",
-              marginBottom: "20;",
-              cursor: "pointer",
-            }}
-          >
-            <DiCssdeck size="3rem" /> <Span>Portfolio</Span>
-          </a>
-        </NavLogo>
+       <NavLogo to="/"
+  style={{
+    display: "flex",
+    alignItems: "center",
+    color: "white",
+    marginBottom: "20px",
+    cursor: "pointer",
+  }}
+>
+  <DiCssdeck size="3rem" /> <Span>Portfolio</Span>
+</NavLogo>
         <MobileIcon>
           <FaBars
             onClick={() => {
